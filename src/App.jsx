e11ae17332b5e691310393ecf0ce6cda7317d0ff
LuisDavidJM <luisdavidjma@gmail.com>
2024-09-12 10:48:18 -0600

    Create Routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
+import Home from './Pages/Home';
+import Contact from './Pages/Contact';
+import Detail from './Pages/Detail';
+import Favs from './Pages/Favs';
 
 
 function App() {
   return (
-      <div className="App">
-          <Navbar/>
-          <Footer/>
-      </div>
+    <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/dentist/:id" element={<Detail />} />
+        <Route path="/favs" element={<Favs />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
+          
   );
 }
 
